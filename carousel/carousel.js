3c948c159227755862e749b3d03958ed7acc8892
Pegggy <fangpei9212@163.com>
2017-08-24 11:03:21 -0500

    一个轮播组件的实现
    1.待改进：新建一个轮播时需要带入img-ct 的顺序
    2.可以把 Carousel 的的接口隐藏起来

---
+++ carousel/carousel.js
@@ -0,0 +1,122 @@
+function Carousel($ct){
+  this.$ct = $ct;
+  this.init();
+  this.bind();
+  this.clock();
+}
+
+Carousel.prototype.init = function(){
+  var $imgList = this.$imgList = this.$ct.find('.img-list');
+  var $imgs = this.$imgs = this.$ct.find('.img-list>li');
+
+  this.$prevBth = this.$ct.find('.prev');
+  this.$nextBth = this.$ct.find('.next');
+  this.$buttons = this.$ct.find('.buttons>li');
+  
+  this.imgWidth = $imgs.width();
+  this.imgLength = $imgs.length;
+  this.imgIndex = 0;
+  this.isAnimate = false;
+
+  $imgList.append($imgs.first().clone());
+  $imgList.prepend($imgs.last().clone());
+
+  $imgList.css({
+  width:(this.imgLength + 2)*this.imgWidth,
+  left: -this.imgWidth});
+}
+
+Carousel.prototype.bind = function(){
+  var _this = this;
+  this.$prevBth.on('click',function(){
+    if(_this.isAnimate){
+      console.log(111);
+      return;
+    }
+    _this.playPrev(1);
+  })
+
+  this.$nextBth.on('click',function(){
+    if(_this.isAnimate){
+      return;
+    }
+    _this.playNext(1);
+  })
+  this.$buttons.click(function(){
+    var len = $(this).index()-_this.imgIndex;
+    console.log(len);
+    if( len > 0){
+      _this.playNext(len);
+    }else if(len < 0){
+      _this.playPrev(-len);
+    }else{
+      return;
+    }
+  })
+
+
+}
+
+Carousel.prototype.playPrev = function(len){
+  var _this = this;
+  this.isAnimate = true;
+  this.$imgList.animate({left:'+=' + _this.imgWidth * len},function(){
+      _this.imgIndex -= len;
+      if(_this.imgIndex < 0){
+        _this.$imgList.css({left: - _this.imgWidth*_this.imgLength});
+        _this.imgIndex = _this.imgLength-1;
+      }
+      _this.showBtn();
+      _this.isAnimate = false;
+  });
+}
+Carousel.prototype.playNext = function(len){
+  var _this = this;
+  this.isAnimate = true;
+  _this.$imgList.animate({left:'-=' + _this.imgWidth * len},function(){
+    _this.imgIndex += len;
+      if(_this.imgIndex === _this.imgLength){
+        _this.$imgList.css({left: -_this.imgWidth});
+        _this.imgIndex = 0;
+      }
+      _this.showBtn();
+    _this.isAnimate = false;
+  });
+}
+
+Carousel.prototype.showBtn = function(){
+ this.$buttons.removeClass('active').eq(this.imgIndex).addClass('active');
+}
+
+Carousel.prototype.clock = function(){
+  var _this = this;
+  var clock = setInterval(function(){
+    _this.playNext(1);
+  },3000);
+
+  this.$ct.on('mouseenter',function(){
+    clearInterval(clock);
+  })
+  this.$ct.on('mouseleave',function(){
+    clock = setInterval(function(){
+    _this.playNext(1);
+  },3000);
+  })  
+}
+new Carousel($('.img-ct'));
+// var clock = setInterval(function(){
+//   playNext(1);
+// },3000);
+
+// $ct.on('mouseenter',function(){
+//   clearInterval(clock);
+// })
+// $ct.on('mouseleave',function(){
+//   clock = setInterval(function(){
+//   playNext(1);
+// },3000);
+// })
+
+
+
+
